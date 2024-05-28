a629823298c733dbf53074682afc2ee1a09b8332
Prashant-zconsolutions <prashant.darunkar@zconsolutions.com>
2024-05-28 14:00:04 +0530

    Lambada expression and Method reference

---
+++ src/java8features/LambdaExpressionEx2.java
@@ -0,0 +1,25 @@
+package java8features;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LambdaExpressionEx2 {
+
+    public static void main(String args[])
+    {
+        List<String> list=new ArrayList<>();
+        list.add("ankit");
+        list.add("mayank");
+        list.add("irfan");
+        list.add("jai");
+
+        list.forEach(
+                (n)->System.out.println(n)
+        );
+
+
+
+    }
+}
